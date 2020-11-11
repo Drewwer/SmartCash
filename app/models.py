7234aebce5f8302361950bea1a457f7b0c05b7c7
Eliaz <eliazdrew@gmail.com>
2020-11-11 02:26:26 -0300

    version3 migramos bd y agregamos superuser cambiamos idioma del panel admin

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Transferencia(models.Model):
+    nombreDeposita = models.CharField(max_length=50)
+    nombreRecibe = models.CharField(max_length=50)
+    monto = models.IntegerField()
+    codigo = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nombreDeposita
+
+
